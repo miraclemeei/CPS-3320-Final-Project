161ce977e3de2bb6e62cfab3b1a5d499fcfcddee
KEVINCHEN的PRO\27671 <zhiyangc@kean.edu>
2022-05-28 19:06:33 +0800

    a

--- python-sentimental-analysis-deep-learning.py
+++ python-sentimental-analysis-deep-learning.py
@@ -1,33 +1,35 @@
-# %%
+# This Code is developed by Zhiyang Chen 1098489
+#Referencing : https://www.youtube.com/watch?v=hprBCp_UJN0
 # import the libraries
 import pandas as pd
 import numpy as np
 import sqlite3
 import keras
 from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.sequence import pad_sequences  
 
 
 # Loading the data from the dataset
 def readData():
     content = sqlite3.connect(r"D:\Documents\Data\archive\database.sqlite")
-    data = pd.read_sql_query(""" SELECT * FROM Reviews""", content)
+    data = pd.read_sql_query(""" SELECT * FROM Reviews""", content) #sql for retrtieve 
     print(data.shape)
     return data
 
-
+# DataCleansing
 def DataCleansing(data):
-    # DataCleansing
+    
     # Cleaning of the duplicated including checking the null values
     df = data.drop_duplicates(subset=['UserId', 'ProfileName', 'Time', 'Text'], keep='first', inplace=False)
+    #If a record gets the 4 attributes same at the same time it will automatically keep the first and delete others
     df.shape
 
     # Start of Building the dataset and for data preparation
     data = df[['Score', 'Text']]  # Slicing the array to omit other information that is essential for analysis
     data['sentiments'] = data.Score.apply(
-        lambda x: 0 if x in [1, 2] else 1)  # using lambda to omit a score of 3, in other words, the neutral
-    data.head()
-    data.drop(data[data['Score'] == 3].index, inplace=True)
+        lambda x: 0 if x in [1, 2] else 1)  # using lambda to create a new column called sentiments, also 
+    data.head() 
+    data.drop(data[data['Score'] == 3].index, inplace=True) # drop the score == 3 as we do not want neural
     data.head()
     return data
 
@@ -66,10 +68,14 @@ def dataMining(data, epoch, batch):
     for row in test_label:
         testing_labels.append(str(row))
 
-    # %%
+    # As I checking the input if the neural network, the list form will not be accepted, so I change the 
+    # list to array
 
     """
     num_words is the dictionary size 
+    oov_token means that any words we go through the str that is out of the num_words, then we represent it as 
+    oov, aka out of vacublary.
+
     """
 
     # <OOV> is Out of Vocabulary total dict length will be 80000
@@ -80,14 +86,18 @@ def dataMining(data, epoch, batch):
     # %%
 
     # padding the length in order to make it a unity form.
+
     sequences = tokenizer.texts_to_sequences(training_sentences)  # change the sequence to a set of the
-    padded = pad_sequences(sequences, maxlen=100, truncating='post', padding='post')
+    padded = pad_sequences(sequences, maxlen=100, truncating='post', padding='post') # the max length for each input paragraph will contain only 100 words, and shorter will be padded
     testing_sentences = tokenizer.texts_to_sequences(testing_sentences)
-    testing_padded = pad_sequences(testing_sentences, maxlen=100, truncating='post', padding='post')
+    testing_padded = pad_sequences(testing_sentences, maxlen=100, truncating='post', padding='post')# anyone longer  than 100 the remaining part will be truncated. 
 
     # %%
 
-    # 1, Word Embedding (same meaning reduction of dimension )2. Golbal average layer, prevent over fitting 3,dense layer 16 units relu 4, output layer
+    # first layer Word Embedding (fraction representation, aim to reduction of dimension) i choose the dimension of the vector to be 9, which means that each words then will be trained for a 9 dimension vector
+    #second layer Golbal average layer, prevent over fitting, and reduction of the dimension, we get the average of the words so that the parameters will be a smaller number. 
+    # third layer dense layer 6 units, means we get 6 nodes in this network a more number of node will denote a more accuracy, relu is the activation function for a sofamx 
+    # final layer output layer, because this problem is a binary classification problem, so that we do not need the image to be higher dimensional, so that, the units or node will simply be 1, and the output should be 0/1, o means negative and 1 means possitive
     model = keras.Sequential([keras.layers.Embedding(80000, 9, input_length=100),  # 9 - deminsion vector
                               keras.layers.GlobalAvgPool1D(),
                               # for getting the average of the word with the same meaning
@@ -132,9 +142,11 @@ def main():
         batch = int(input())
     except:  # if not integer then throw the exception
         print("You Must Input Valid Integer for epochs and batches")
-
+    #data retrieving 
     data = readData()
+    #data preprocessing
     data = DataCleansing(data)
+    # data mining, which require 2 input from the user to set the hyperparameters. 
     dataMining(data, epoch, batch)
 
 if __name__ == "__main__":
